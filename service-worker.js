73c9b2570dae529d0e8e0af168d1dd897151f333
andreyodr33-eng <andreyodr33@gmail.com>
2026-02-23 16:50:21 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "cardvault-v1";
+
+const urlsToCache = [
+  "/vault/",
+  "/vault/index.html",
+  "/vault/manifest.json",
+  "/vault/icon-192.png",
+  "/vault/icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"
+];
+
+// Установка
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Активация
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Перехват запросов
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
